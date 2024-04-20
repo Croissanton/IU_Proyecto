ffec195ce72e3ae73c8298a575f433c11e307080
Ignacy Borzestowski <ignacy.borzestowski@gmail.com>
2024-04-20 20:59:27 +0200

    initial commit

---
+++ foro-mundo/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './Components/Header.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+      </header>
+    </div>
+  );
+}
+
+export default App;
