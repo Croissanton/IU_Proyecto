f523b8c3c0af43464d88e0b60753bf35e899f917
Ignacy Borzestowski <ignacy.borzestowski@gmail.com>
2024-04-21 19:28:13 +0200

    links to posts

--- foro-mundo/src/Components/PostCard.js
+++ foro-mundo/src/Components/PostCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const PostCard = ({
   topic,
@@ -11,52 +12,54 @@ const PostCard = ({
   lm_date,
 }) => {
   return (
-    <Row className="gy-3">
-      <Col className="p-3 m-auto">
-        <Container className="border border-dark-subtle bg-light">
-          <Row>
-            <Col className="border-end border-dark-subtle p-3">
-              <Row>
-                <Col>
-                  <Row>
-                    <h4>{topic}</h4>
-                  </Row>
-                  <Row>
-                    <p>{author}</p>
-                  </Row>
-                  <Row>
-                    <p>{date}</p>
-                  </Row>
-                </Col>
-              </Row>
-            </Col>
-            <Col className="p-3">
-              <Row>
-                <Col className="m-auto">
-                  <h5>Respuestas</h5>
-                  <p>{res_num}</p>
-                </Col>
-                <Col className="m-auto">
-                  <h5>Número de visualizaciones</h5>
-                  <p>{view_num}</p>
-                </Col>
-                <Col className="m-auto">
-                  <Row>
-                    <p>Último mensaje por:</p>
-                  </Row>
-                  <Row>
-                    <p>{lm_author}</p>
-                  </Row>
-                  <Row>
-                    <p>{lm_date}</p>
-                  </Row>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Container>
-      </Col>
-    </Row>
+    <Link to={`/post/${topic}`} className="custom-link">
+      <Row className="gy-3">
+        <Col className="p-3 m-auto ">
+          <Container className="border border-dark-subtle bg-light">
+            <Row className="custom-link-container">
+              <Col className="border-end border-dark-subtle p-3">
+                <Row>
+                  <Col>
+                    <Row>
+                      <h4>{topic}</h4>
+                    </Row>
+                    <Row>
+                      <p>{author}</p>
+                    </Row>
+                    <Row>
+                      <p>{date}</p>
+                    </Row>
+                  </Col>
+                </Row>
+              </Col>
+              <Col className="p-3">
+                <Row>
+                  <Col className="m-auto">
+                    <h5>Respuestas</h5>
+                    <p>{res_num}</p>
+                  </Col>
+                  <Col className="m-auto">
+                    <h5>Número de visualizaciones</h5>
+                    <p>{view_num}</p>
+                  </Col>
+                  <Col className="m-auto">
+                    <Row>
+                      <p>Último mensaje por:</p>
+                    </Row>
+                    <Row>
+                      <p>{lm_author}</p>
+                    </Row>
+                    <Row>
+                      <p>{lm_date}</p>
+                    </Row>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
+        </Col>
+      </Row>
+    </Link>
   );
 };
 
--- foro-mundo/src/index.js
+++ foro-mundo/src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import ProfilePage from './Pages/ProfilePage';
-import SearchPage from './Pages/SearchPage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProfilePage from "./Pages/ProfilePage";
+import SearchPage from "./Pages/SearchPage";
 
 const router = createBrowserRouter([
   {
@@ -23,13 +20,12 @@ const router = createBrowserRouter([
     path: "/search",
     element: <SearchPage />,
   },
-
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
