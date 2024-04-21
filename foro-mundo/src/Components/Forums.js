38aeec52b3940ff6b11653ee30fd48a15d25e96c
Ignacy Borzestowski <ignacy.borzestowski@gmail.com>
2024-04-21 17:43:22 +0200

    asdf:D

--- foro-mundo/src/Components/Forums.js
+++ foro-mundo/src/Components/Forums.js
@@ -1,17 +1,57 @@
 import React from "react";
-import Post from "./Post";
+import PostCard from "./PostCard";
 
-const Threads = () => {
+const Forums = () => {
   return (
-    <div className="container-xxl">
+    <div className="container-xxl my-3">
       <h2>Foros</h2>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <PostCard
+        topic={"Foro coche es una mierda"}
+        author={"Juan Jaun"}
+        date={"18.10.1992"}
+        lm_author={"Jose Jose"}
+        lm_date={"19.11.2002"}
+        res_num={100}
+        view_num={1000}
+      />
+      <PostCard
+        topic={"Por que me ha explotado el radiador??"}
+        author={"Ignacio19291"}
+        date={"10.04.2024"}
+        res_num={69}
+        view_num={569}
+        lm_author={"Mikixd"}
+        lm_date={"10.04.2024"}
+      />
+      <PostCard
+        topic={"Mi gato se ha comido a mi abuela :( que hago"}
+        author={"Ignacio19291"}
+        date={"10.04.2024"}
+        res_num={69}
+        view_num={569}
+        lm_author={"usuarionumeritos1234235345i"}
+        lm_date={"10.04.2024"}
+      />
+      <PostCard
+        topic={"Mi abuela se ha comido a mi gato.........!!"}
+        author={"percebe43"}
+        date={"10.04.2024"}
+        res_num={69}
+        view_num={569}
+        lm_author={"Mikixd"}
+        lm_date={"10.04.2024"}
+      />
+      <PostCard
+        topic={"La gasolina casi a 2€"}
+        author={"Ignacio19291"}
+        date={"10.04.2024"}
+        res_num={69}
+        view_num={569}
+        lm_author={"Mikixd"}
+        lm_date={"10.04.2024"}
+      />
     </div>
   );
 };
 
-export default Threads;
+export default Forums;
---
+++ foro-mundo/src/Components/PostCard.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const PostCard = ({
+  topic,
+  author,
+  date,
+  res_num,
+  view_num,
+  lm_author,
+  lm_date,
+}) => {
+  return (
+    <Row className="gy-3">
+      <Col className="p-3 m-auto">
+        <Container className="border border-dark-subtle bg-light">
+          <Row>
+            <Col className="border-end border-dark-subtle p-3">
+              <Row>
+                <Col>
+                  <Row>
+                    <h4>{topic}</h4>
+                  </Row>
+                  <Row>
+                    <p>{author}</p>
+                  </Row>
+                  <Row>
+                    <p>{date}</p>
+                  </Row>
+                </Col>
+              </Row>
+            </Col>
+            <Col className="p-3">
+              <Row>
+                <Col className="m-auto">
+                  <h5>Respuestas</h5>
+                  <p>{res_num}</p>
+                </Col>
+                <Col className="m-auto">
+                  <h5>Number of Views</h5>
+                  <p>{view_num}</p>
+                </Col>
+                <Col className="m-auto">
+                  <Row>
+                    <p>Ultimo mensaje:</p>
+                  </Row>
+                  <Row>
+                    <p>{lm_author}</p>
+                  </Row>
+                  <Row>
+                    <p>{lm_date}</p>
+                  </Row>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
+
+export default PostCard;
