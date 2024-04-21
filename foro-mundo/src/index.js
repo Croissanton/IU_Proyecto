041f5600a866153b48d32a6edae7cd6b5983da52
Ignacy Borzestowski <ignacy.borzestowski@gmail.com>
2024-04-21 20:25:59 +0200

    pagina de error

--- foro-mundo/src/index.js
+++ foro-mundo/src/index.js
@@ -7,6 +7,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ProfilePage from "./Pages/ProfilePage";
 import SearchPage from "./Pages/SearchPage";
 import PostPage from "./Pages/PostPage";
+import ErrorPage from "./Pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
@@ -24,6 +25,10 @@ const router = createBrowserRouter([
   {
     path: "/post",
     element: <PostPage />,
+  },
+  {
+    path: "*",
+    element: <ErrorPage />,
   },
 ]);
 
