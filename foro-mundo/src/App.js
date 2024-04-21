5548f65945ccf30ce8e38f4977328f6f35c66879
unknown <crisruizmartin1@gmail.com>
2024-04-21 16:42:12 +0200

    App changed, created MainPage.js

--- foro-mundo/src/App.js
+++ foro-mundo/src/App.js
@@ -1,16 +1,12 @@
 import "./App.css";
 import "./css/custom.css";
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
-import Header from "./Components/Header.js";
-import Footer from "./Components/Footer.js";
-import Forums from "./Components/Forums.js";
+import MainPage from "./Pages/MainPage";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Forums />
-      <Footer />
+      <MainPage />
     </div>
   );
 }
---
+++ foro-mundo/src/Pages/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MainLayout from "../layout/MainLayout.js";
+import Forums from "../Components/Forums.js";
+
+function ProfilePage() {
+  return (
+    <MainLayout>
+      <Forums />
+    </MainLayout>
+  );
+}
+
+export default ProfilePage;
