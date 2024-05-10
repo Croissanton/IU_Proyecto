1cfbe4b9b04b6ab45680fe5c3638597c402140cd
Pablo <pabloruar03@uma.es>
2024-05-10 16:51:12 +0200

    Perfil acabado
    editar y to

--- foro-mundo/src/Pages/ProfilePage.js
+++ foro-mundo/src/Pages/ProfilePage.js
@@ -1,7 +1,39 @@
 import MainLayout from "../layout/MainLayout.js";
-import React from "react";
+import React, {useState} from "react";
 
 function ProfilePage() {
+  const [isEditing, setIsEditing] = useState(false);
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const inputs = document.querySelectorAll('input[required], textarea[required]');
+    let formIsValid = true;
+  
+    inputs.forEach(input => {
+      if (!input.value.trim()) {
+        formIsValid = false;
+        // Puedes agregar algún mensaje de error o estilo para indicar que el campo es requerido
+        input.classList.add('is-invalid');
+      } else {
+        input.classList.remove('is-invalid');
+      }
+    });
+  
+    if (formIsValid) {
+      setFormSubmitted(true);
+      setIsEditing(false);
+    }
+  };
+
+  if (formSubmitted) {
+    window.location.reload();
+  }
+
   return (
     <MainLayout>
       <h1>Mi Perfil</h1>
@@ -24,6 +56,7 @@ function ProfilePage() {
             <button
               type="secondary"
               className="btn btn-primary"
+              style={{ display: isEditing ? "block" : "none" }}
               onClick={() => document.getElementById("imageInput").click()}
             >
               Cambiar foto
@@ -44,6 +77,7 @@ function ProfilePage() {
                 className="form-control"
                 id="inputNombre4"
                 placeholder="Tu nombre"
+                readOnly={!isEditing}
               ></input>
             </div>
             <div className="col-md-6">
@@ -55,6 +89,7 @@ function ProfilePage() {
                 className="form-control"
                 id="inputApellidos4"
                 placeholder="Tus apellidos"
+                readOnly={!isEditing}
               ></input>
             </div>
             <div className="col-12">
@@ -66,6 +101,7 @@ function ProfilePage() {
                 className="form-control"
                 id="inputFechaNacimiento"
                 placeholder="Tu fecha de nacimiento en formato dd/mm/aaaa"
+                readOnly={!isEditing}
               ></input>
             </div>
             <div className="col-md-6">
@@ -77,6 +113,7 @@ function ProfilePage() {
                 className="form-control"
                 id="inputPais"
                 placeholder="El nombre de tu pais"
+                readOnly={!isEditing}
               ></input>
             </div>
             <div className="col-md-6">
@@ -88,13 +125,20 @@ function ProfilePage() {
                 className="form-control"
                 id="inputCiudad"
                 placeholder="El nombre de tu ciudad"
+                readOnly={!isEditing}
               ></input>
             </div>
             <div className="col-md-12">
               <label htmlFor="inputRedes" className="form-label">
                 Redes
               </label>
-              <textarea required className="form-control" id="inputRedes" placeholder="Describe cómo encontrarte en las redes sociales"></textarea>
+              <textarea required 
+              className="form-control" 
+              id="inputRedes" 
+              placeholder="Describe cómo encontrarte en las redes sociales"
+              readOnly={!isEditing}
+              >
+              </textarea>
             </div>
             <div className="col-12">
               <label htmlFor="inputDescripcion" className="form-label">
@@ -105,11 +149,39 @@ function ProfilePage() {
                 id="inputDescripcion"
                 rows="3"
                 placeholder="Tu descripción de perfil"
+                readOnly={!isEditing}
               ></textarea>
             </div>
             <div className="col-12">
-              <button type="submit" className="btn btn-primary">
+              <button
+                type="button"
+                className="btn btn-primary"
+                style={{ display: isEditing ? "none" : "block" }}
+                onClick={handleEdit}
+              >
+                Editar
+              </button>
+              <button 
+              type="submit" 
+              className="btn btn-primary"
+              style={{ display: isEditing ? "block" : "none" }}
+              onClick={handleSubmit}
+              >
                 Guardar
+              </button>
+            </div>
+            <div className="col-12">
+              <button
+                type="button"
+                className="btn btn-primary"
+                style={{ display: isEditing ? "block" : "none" }}
+                onClick={() => {
+                  setIsEditing(false)
+                  window.location.reload();
+                  } 
+                }
+              >
+                Cancelar
               </button>
             </div>
           </form>
