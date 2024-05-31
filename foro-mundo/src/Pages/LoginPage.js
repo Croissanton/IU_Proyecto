0c3c32e3dbe0d5099635511c66741624eac48c08
unknown <crisruizmartin1@gmail.com>
2024-05-31 16:35:47 +0200

    signup and login accessibility improved

--- foro-mundo/src/Pages/LoginPage.js
+++ foro-mundo/src/Pages/LoginPage.js
@@ -50,11 +50,11 @@ function LoginPage() {
             <p>Por favor, inicia sesión para continuar.</p>
             <div className="mb-3">
               <label htmlFor="username" className="form-label">Nombre de usuario</label>
-              <input type="text" className="form-control form-control-sm" required value={username} onChange={(e) => setUsername(e.target.value)} style={{ marginBottom: "0.5rem", width: "300px", margin: "0 auto" }} />
+              <input type="text" className="form-control form-control-sm" aria-label="nombre_de_usuario" required value={username} onChange={(e) => setUsername(e.target.value)} style={{ marginBottom: "0.5rem", width: "300px", margin: "0 auto" }} />
             </div>
             <div className="mb-3">
               <label htmlFor="password" className="form-label">Contraseña</label>
-              <input type="password" className="form-control form-control-sm" required value={password} onChange={(e) => setPassword(e.target.value)} style={{ marginBottom: "0.5rem", width: "300px", margin: "0 auto" }} />
+              <input type="password" className="form-control form-control-sm" aria-label="contraseña" required value={password} onChange={(e) => setPassword(e.target.value)} style={{ marginBottom: "0.5rem", width: "300px", margin: "0 auto" }} />
             </div>
             <button type="submit" className="btn btn-primary text-secondary border border-secondary-subtle m-3">Iniciar Sesión</button>
             <button type="button" onClick={() => window.location.href = "/register" } className="btn btn-primary text-secondary border border-secondary-subtle m-3">Registrarse</button>
--- foro-mundo/src/Pages/SignUpPage.js
+++ foro-mundo/src/Pages/SignUpPage.js
@@ -36,8 +36,8 @@ function SignUpPage() {
     <div className="container-fluid d-flex justify-content-center align-items-center border border-dark-subtle bg-light" style={{ minHeight: "100vh", backgroundColor: "#f8f9fa" }}>
       <div className="p-4" style={{ maxWidth: "400px", background: "#ececec", borderRadius: "8px", boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.1)" }}>
         <Breadcrumb>
-          <Breadcrumb.Item href="../#">Inicio</Breadcrumb.Item>
-          <Breadcrumb.Item active>Registro</Breadcrumb.Item>
+          <Breadcrumb.Item href="../#" aria-label="enlace_a_inicio">Inicio</Breadcrumb.Item>
+          <Breadcrumb.Item active aria-label="enlace_a_registro">Registro</Breadcrumb.Item>
         </Breadcrumb>
         <form className="row col-12 g-3" onSubmit={register} action="/">
             <div className="login-container text-center">
@@ -46,35 +46,35 @@ function SignUpPage() {
             <p>Por favor, completa el siguiente formulario para registrarte.</p>
             <div className="mb-3">
                 <label htmlFor="username" className="form-label">Nombre de usuario</label>
-                <input type="text" className="form-control form-control-sm" name="username" value={user.username} onChange={handleInputChange} required />
+                <input type="text" className="form-control form-control-sm" name="username" value={user.username} onChange={handleInputChange} required aria-label="nombre_usuario"/>
                 <label htmlFor="password" className="form-label">Contraseña</label>
-                <input type="password" className="form-control form-control-sm" name="password" value={user.password} onChange={handleInputChange} required />
+                <input type="password" className="form-control form-control-sm" name="password" value={user.password} onChange={handleInputChange} required aria-label="contraseña"/>
                 <label htmlFor="name" className="form-label">Nombre</label>
-                <input type="text" className="form-control form-control-sm" name="name" value={user.name} onChange={handleInputChange} required />
+                <input type="text" className="form-control form-control-sm" name="name" value={user.name} onChange={handleInputChange} required aria-label="nombre"/>
             </div>
             <div className="mb-3">
-                <label htmlFor="lastName" className="form-label">Apellido</label>
-                <input type="text" className="form-control form-control-sm" name="lastName" value={user.lastName} onChange={handleInputChange} required />
+                <label htmlFor="lastName" className="form-label">Apellidos</label>
+                <input type="text" className="form-control form-control-sm" name="lastName" value={user.lastName} onChange={handleInputChange} required aria-label="apellidos" />
             </div>
             <div className="mb-3">
                 <label htmlFor="birthDate" className="form-label">Fecha de Nacimiento</label>
-                <input type="date" className="form-control form-control-sm" name="birthDate" value={user.birthDate} onChange={handleInputChange} required />
+                <input type="date" className="form-control form-control-sm" name="birthDate" value={user.birthDate} onChange={handleInputChange} required aria-label="fecha_de_nacimiento"/>
             </div>
             <div className="mb-3">
                 <label htmlFor="country" className="form-label">País</label>
-                <input type="text" className="form-control form-control-sm" name="country" value={user.country} onChange={handleInputChange} required/>
+                <input type="text" className="form-control form-control-sm" name="country" value={user.country} onChange={handleInputChange} required aria-label="pais"/>
             </div>
             <div className="mb-3">
                 <label htmlFor="city" className="form-label">Ciudad (Opcional)</label>
-                <input type="text" className="form-control form-control-sm" name="city" value={user.city} onChange={handleInputChange}/>
+                <input type="text" className="form-control form-control-sm" name="city" value={user.city} onChange={handleInputChange} aria-label="ciudad_opcional"/>
             </div>
             <div className="mb-3">
                 <label htmlFor="socialMedia" className="form-label">Redes Sociales (Opcional)</label>
-                <input type="text" className="form-control form-control-sm" name="socialMedia" value={user.socialMedia} onChange={handleInputChange}/>
+                <input type="text" className="form-control form-control-sm" name="socialMedia" value={user.socialMedia} onChange={handleInputChange} aria-label="redes_sociales_opcional"/>
             </div>
             <div className="mb-3">
                 <label htmlFor="description" className="form-label">Descripción (Opcional)</label>
-                <textarea className="form-control form-control-sm" name="description" value={user.description} onChange={handleInputChange}/>
+                <textarea className="form-control form-control-sm" name="description" value={user.description} onChange={handleInputChange} aria-label="descripcion_opcional"/>
             </div>
             <button type="submit" className="btn btn-primary text-secondary border border-secondary-subtle m-3">Registrarse</button>
             <BackButton />
